26a3ef20eb7636163f7b87a2877c2a76610e2c66
victorN <victornguyen082@gmail.com>
2019-11-13 15:26:34 -0500

    binary search tree day 1

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,8 @@
-import sys
-sys.path.append('../queue_and_stack')
-from dll_queue import Queue
 from dll_stack import Stack
+from dll_queue import Queue
+import sys
+import io
+sys.path.append('./queue_and_stack')
 
 
 class BinarySearchTree:
@@ -12,21 +13,54 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # < Go left
+        # >= go right
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # To search a given key in BTS, we first compare with root
+        # if the key is present at root, we return root. If key is greater than root's key,
+        # we recur for right subtree of root node. Otherwise we recur for left subtree.
+        if self.value is target:
+            return True
+        # else if target > node, go right and then have target < node go left
+        elif not self.right and not self.left:
+            return False
+        elif target >= self.value:
+            return self.right.contains(target)
+        else:
+            return self.left.contains(target)
 
     # Return the maximum value found in the tree
+
     def get_max(self):
-        pass
+        # Go right until you cannot go right no further
+        if not self.right:
+            return self.value
+        return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        # visit every node exactly once
+        # go left until you can't anymore, then
+        # go back and go right
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
